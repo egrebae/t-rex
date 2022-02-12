60e09fdcaa138097c763ee38993eacf162a1e6f9
egrebae <96670128+egrebae@users.noreply.github.com>
2022-02-12 11:22:13 +0700

    Add files via upload

---
+++ EU AVN X16RT.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex  -a x16rt -o stratum+tcp://stratum.coinminerz.com:3374 -u RWcW7YK4G2BpETCtP9fRg8qUHRW9vcus2u.w1 -p x
---
+++ SG AVN X16RT.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex  -a x16rt -o stratum+tcp://sg-stratum.coinminerz.com:3374 -u RWcW7YK4G2BpETCtP9fRg8qUHRW9vcus2u.w1 -p x
